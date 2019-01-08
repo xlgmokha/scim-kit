6b7102e3eb51c178d4d5da9f7aee62666adbb214
mokha <mokha@cisco.com>
2019-01-08 11:19:23 -0700

    extract v2 file

---
+++ lib/scim/kit/v2.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'scim/kit/v2/attributable'
+require 'scim/kit/v2/attribute'
+require 'scim/kit/v2/attribute_type'
+require 'scim/kit/v2/authentication_scheme'
+require 'scim/kit/v2/messages'
+require 'scim/kit/v2/meta'
+require 'scim/kit/v2/mutability'
+require 'scim/kit/v2/resource'
+require 'scim/kit/v2/resource_type'
+require 'scim/kit/v2/returned'
+require 'scim/kit/v2/schema'
+require 'scim/kit/v2/schemas'
+require 'scim/kit/v2/service_provider_configuration'
+require 'scim/kit/v2/supportable'
+require 'scim/kit/v2/uniqueness'
+
+module Scim
+  module Kit
+    # Version 2 of the SCIM RFC https://tools.ietf.org/html/rfc7644
+    module V2
+    end
+  end
+end
