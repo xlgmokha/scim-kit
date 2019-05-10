6e5dc206dfce399e231e6f35f77e4c55bd547468
mokha <mokha@cisco.com>
2019-05-10 11:04:21 -0600

    add spec for each operator

--- spec/scim/kit/v2/parser_spec.rb
+++ spec/scim/kit/v2/parser_spec.rb
@@ -3,13 +3,15 @@
 RSpec.describe Scim::Kit::V2::Parser do
   subject { described_class.new }
 
-  context "simple expression" do
-    let(:expression) { %Q(userName eq "bjensen") }
-    let(:result) { subject.pretty_parse(expression) }
+  [ "eq", "ne", "co", "sw", "ew", "gt", "lt", "ge", "le" ].each do |operator|
+    context "simple expression #{operator}" do
+      let(:expression) { %Q(userName #{operator} "bjensen") }
+      let(:result) { subject.pretty_parse(expression) }
 
-    specify { expect(result).to be_present }
-    specify { expect(result[:left].to_s).to eql('userName') }
-    specify { expect(result[:operator].to_s).to eql('eq') }
-    specify { expect(result[:right].to_s).to eql('bjensen') }
+      specify { expect(result).to be_present }
+      specify { expect(result[:left].to_s).to eql('userName') }
+      specify { expect(result[:operator].to_s).to eql(operator) }
+      specify { expect(result[:right].to_s).to eql('bjensen') }
+    end
   end
 end
